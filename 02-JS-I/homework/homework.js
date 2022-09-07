98cf75046ae6e2af6608c95fe5e0ddf5f850891e
Karim A Martinez <kam980420@gmail.com>
2022-09-06 19:41:02 -0700

    respondi mas preguntas

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -27,14 +27,16 @@ const nuevoModulo = 21 % 5 === 1;
 
 function devolverString(str) {
   // "Return" la string provista: str
-  // Tu código:
+  // Tu código: 
+  return str;
   
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
-  // Tu código:
+  // Tu código: 
+ return x = y;
   
 }
 
